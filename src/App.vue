30b40c5927afb95323e214cfe69906d2abf28975
Mathias Strathausen <m.strathausen@gmail.com>
2021-07-14 22:18:03 +0200

    Adding some text to the portfolio.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="container mx-auto my-8 lg:w-10/12">
     <header class="flex items-end">
-      <span class="mr-3 bg-yellow-500 p-2 text-white">STR</span>
-      <nav>
-        <a href="" class="mx-3 text-2xl" v-for="name of ['about', 'projects']" v-bind:key="name">{{name}}</a>
+      <span class="mr-3 bg-blue-100 p-2 text-white">STR</span>
+      <nav class="text-3xl">
+        <a href="" class="mx-3" v-for="name of ['about', 'projects']" v-bind:key="name">{{name}}</a>
       </nav>
     </header>
     <div class="my-20 text-xl">
       <h1 class="font-serif font-bold text-6xl mb-6">Moin, I'm Mathias!</h1>
       <p>
         I'm a Senior Fullstack Developer from Hamburg, Germany.
+        I love solving problems and working with people to do that. <br>
+<!--        My ideal day consists of working on quality software (or developing it to be that way), broadening my own knowledge and that of the people I work with.-->
       </p>
+      <h2>What I mainly do:</h2>
+      <ul>
+        <li>Typescript</li>
+        <li>Node.js</li>
+        <li>Vue.js</li>
+      </ul>
     </div>
     <footer class="flex">
-      <a v-bind:href=item.url class="mx-3" v-for="item of footerItems" v-bind:key="item.title">
+      <a v-bind:href=item.url target="_blank" class="mx-3" v-for="item of footerItems" v-bind:key="item.title">
         {{ item.text }}
       </a>
     </footer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@ module.exports = {
   purge: ['./index2.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      orange: '#EA7317',
+      yellow: '#FEC601',
+      black: '#443d3d',
+      white: '#fff',
+      blue: {
+        100: '#2364AA',
+        300: '#3DA5D9',
+        600: '#73BFB8',
+      },
+    },
     extend: {
       fontFamily: {
         serif: ['Noto Serif', 'serif'],
